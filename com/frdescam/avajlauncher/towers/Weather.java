93f30f3296e70f013b608ce4e8f91a9214598f44
fdeĉ <fd@42l.fr>
2025-01-12 23:15:19 +0100

    remove useless methods in Weather enum

--- com/frdescam/avajlauncher/towers/Weather.java
+++ com/frdescam/avajlauncher/towers/Weather.java
@@ -4,25 +4,5 @@ public enum Weather {
     RAIN,
     FOG,
     SUN,
-    SNOW;
-
-    public static Weather fromString(String string)
-    {
-        for (Weather weather : Weather.values()) {
-            if (weather.name().equalsIgnoreCase(string)) {
-                return weather;
-            }
-        }
-        throw new IllegalArgumentException("Unknown weather : " + string);
-    }
-
-    // public static Weather toString(Weather weatherEnum)
-    // {
-    //     for (Weather weather : Weather.values()) {
-    //         if (weather.name().equalsIgnoreCase(weatherEnum.toString())) {
-    //             return weather;
-    //         }
-    //     }
-    //     throw new IllegalArgumentException("Unknown weather : " + weatherEnum);
-    // }
+    SNOW
 }
